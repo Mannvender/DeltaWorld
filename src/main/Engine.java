3a1d014c73c90902848e4bc912b0de43cdcb3605
RoBo_ <mannvender001@gmail.com>
2017-06-30 09:59:52 +0530

    #class ZorkTrigger written  not so clear what it does but it contains conditions AL,  type of trigger(by default SINGLE), hasCommand bool ,print and Action AL's #class ZorkRoom written #class ZorkItem written #class ZorkContainer written #class ZorkCreature written

--- src/main/Engine.java
+++ src/main/Engine.java
@@ -1,6 +1,8 @@
 package main;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 // generic object ,everything inherits from it.
 class ZorkObject {
@@ -57,7 +59,7 @@ class ZorkConditionHas extends ZorkCondition {
 			ZorkRoom roomObject = zork.Rooms.get(owner);
 			if (roomObject != null) {
 				if ((roomObject).item.get(object) != null && has.equals("yes")
-						|| (roomObject).get.item.get(object) == null && has.equals("no")) {
+						|| (roomObject).item.get(object) == null && has.equals("no")) {
 					return true;
 				} else {
 					return false;
@@ -98,7 +100,90 @@ class ZorkCommand extends ZorkCondition {
 
 // trigger
 class ZorkTrigger {
+	public ArrayList<ZorkCondition> conditions = new ArrayList<ZorkCondition>();
+	String type = "single"; // by default single
+	boolean hasCommand = false;
+	public ArrayList<String> action = new ArrayList<>();
+	public ArrayList<String> print = new ArrayList<>();
 
+	public boolean evaluate(Engine zork) {
+		for (int i = 0; i < conditions.size(); i++) {
+			if (!conditions.get(i).evaluate(zork)) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+}
+
+// room
+class ZorkRoom extends ZorkObject {
+	public String name;
+	public String type = "regular";
+	public String description;
+	public HashMap<String, String> border = new HashMap<>();
+	public HashMap<String, String> container = new HashMap<>();
+	public HashMap<String, String> item = new HashMap<>();
+	public HashMap<String, String> creature = new HashMap<>();
+
+	public ZorkRoom() {
+
+	}
+}
+
+// item
+class ZorkItem extends ZorkObject {
+	public String name;
+	public String description;
+	public String writing;
+	public ArrayList<String> turnOnAction = new ArrayList<>();
+	public ArrayList<String> turnOnPoint = new ArrayList<>();
+
+	public ZorkItem() {
+
+	}
+}
+
+// container
+class ZorkContainer extends ZorkObject {
+	public String name;
+	public String description;
+	boolean isOpen;
+	public ArrayList<String> accept = new ArrayList<>();
+	public HashMap<String, String> item = new HashMap<>();
+
+	public ZorkContainer() {
+
+	}
+}
+
+// creature
+class ZorkCreature extends ZorkObject {
+	public String name;
+	public String description;
+	public HashMap<String, String> vulnerability = new HashMap<>();
+	public ArrayList<ZorkCondition> conditions = new ArrayList<ZorkCondition>();
+	public ArrayList<String> print = new ArrayList<>();
+	public ArrayList<String> action = new ArrayList<>();
+
+	public ZorkCreature() {
+
+	}
+
+	// to evaluate status of attack
+	public boolean attack(Engine zork, String weapon) {
+		if (vulnerability.get(weapon) == null) {
+			return false;
+		}
+		for (int i = 0; i < conditions.size(); i++) {
+			if (!conditions.get(i).evaluate(zork)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 public class Engine {
